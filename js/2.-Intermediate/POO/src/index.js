025fcd219b522072571a945d9685ef1dff8f47b4
erickd3v <moyae76@gmail.com>
2023-09-29 00:58:16 -0500

    ok

---
+++ js/2.-Intermediate/POO/src/index.js
@@ -0,0 +1,21 @@
+class Product {
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI {
+    addProduct() {
+
+    }
+
+    deleteProduct() {
+
+    }
+
+    showMessage() {
+        
+    }
+}
